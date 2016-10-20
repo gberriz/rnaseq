5f6a5e12e280aee101872a8df7eada4f33f761c8
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2016-10-20 10:45:26 -0400

    tabulate: multiple enhancements

--- simulation/src/tabulate.py
+++ simulation/src/tabulate.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
 import os
 import collections
@@ -14,21 +17,49 @@ import misc
 HALFINDENT = '  '
 
 class Row(object):
-    def __init__(self, depth,
-                 colnames=[None], cells={}, label='', is_header=False):
+    def __init__(self,
+                 depth,
+                 is_singleton,
+                 colnames=[None],
+                 cells={},
+                 label='',
+                 tooltip=None,
+                 is_header=False):
+
+        # h = is_header
+        # s = is_singleton
+        # t = bool(tooltip)
+        # f = (h or not s)
+        # g = (t == f)
+
+        # h s t   f g
+        # F F F ✗ T F
+        # F F T ✓ T T
+        # F T F ✓ F T
+        # F T T ✗ F F
+        # T F F ✗ T F
+        # T F T ✓ T T
+        # T T F ✗ T F
+        # T T T ✓ T T
+
+        assert bool(tooltip) == (is_header or not is_singleton)
 
         assert not ((cells or label) and is_header)
+        assert (is_singleton or is_header) == (not label)
 
         assert set(cells.keys()).issubset(set(colnames))
 
         self.depth = depth
+        self.is_singleton = is_singleton
         self.colnames = colnames
         self.cells = cells
         self.label = '' if not label else label
+        self.tooltip = tooltip
         self.is_header = is_header
 
         self.indent = HALFINDENT * 2 * depth
         self.cell_tag = 'th' if is_header else 'td'
+
 
     def __str__(self):
         indent = self.indent
@@ -38,16 +69,34 @@ class Row(object):
         ret = []
 
         ret.append('%s<tr class="depth-%d">\n' % (indent, self.depth))
-        label_cell = ('%s%s<%s>%s</%s>\n' %
-                      (indent, HALFINDENT, cell_tag, self.label, cell_tag))
 
-        ret.append(label_cell)
+        if self.is_singleton:
+            label_cell = []
+        elif self.is_header:
+            label_cell = [('%s%s<%s></%s>\n' %
+                           (indent, HALFINDENT, cell_tag, cell_tag))]
+        else:
+            # label_cell = [('%s%s<%s><span class="header" title="%s">%s</span></%s>\n' %
+            #                (indent, HALFINDENT, cell_tag, self.tooltip,
+            #                 self.label, cell_tag))]
 
+            label_cell = [('%s%s<%s class="header" title="%s">%s</%s>\n' %
+                           (indent, HALFINDENT, cell_tag, self.tooltip,
+                            self.label, cell_tag))]
+
+
+        ret.extend(label_cell)
 
         if self.is_header:
+
             for colname in self.colnames:
-                ret.append('%s%s<%s>%s</%s>\n' %
-                           (indent, HALFINDENT, cell_tag, colname, cell_tag))
+                # ret.append('%s%s<%s><span class="header" title="%s">%s</span></%s>\n' %
+                #            (indent, HALFINDENT, cell_tag,
+                #             self.tooltip, colname, cell_tag))
+
+                ret.append('%s%s<%s class="header" title="%s">%s</%s>\n' %
+                           (indent, HALFINDENT, cell_tag,
+                            self.tooltip, colname, cell_tag))
         else:
 
             for colname in self.colnames:
@@ -56,7 +105,7 @@ class Row(object):
                 ret.append('%s%s</%s>\n' % (indent, HALFINDENT, cell_tag))
 
 
-        ret.append(label_cell)
+        ret.extend(label_cell)
 
         ret.append('%s</tr>\n' % indent)
 
@@ -146,19 +195,32 @@ def tabulate(dataframe, columns, callback, orientation='V'):
                                                    max_depth=max_depth))
                               for key, subdf in groups.items()]))
 
+        number_of_cells = len(cells.keys())
+        assert number_of_cells > 0
+
         if orientation == 'V':
 
-            rows = [Row(depth + 1, cells={None: content}, label=key)
-                    for key, content in cells.items()]
+            is_singleton = number_of_cells == 1
+            tooltip = '' if is_singleton else columns[0]
 
+            rows = [Row(depth + 1,
+                        is_singleton=is_singleton,
+                        cells={None: content},
+                        tooltip=tooltip,
+                        label=key)
+                    for key, content in cells.items()]
 
         else:
 
             assert orientation == 'H'
             colnames = cells.keys()
-            header = Row(depth + 1, colnames=colnames, is_header=True)
+            header = Row(depth + 1, is_singleton=True,
+                         colnames=colnames,
+                         tooltip=columns[0],
+                         is_header=True)
             rows = [header,
-                    Row(depth + 1, colnames=colnames, cells=cells),
+                    Row(depth + 1, is_singleton=True,
+                        colnames=colnames, cells=cells),
                     header]
 
         return Table(depth, rows=rows)
@@ -174,15 +236,29 @@ def tabulate(dataframe, columns, callback, orientation='V'):
 
         groups = get_groups(dataframe, columns[0])
 
+        number_of_rows = len(groups.keys())
+        assert number_of_rows > 0
+
+        is_singleton = number_of_rows == 1
+
         colname_set = set()
         for subdf in groups.values():
             colname_set.update(get_groups(subdf, columns[1]).keys())
 
         colnames = sorted(colname_set)
-        header_row = Row(depth + 1, colnames=colnames, is_header=True)
+
+
+        if len(colnames) < 2:
+            header_row = []
+        else:
+            header_row = [Row(depth + 1,
+                              is_singleton,
+                              colnames=colnames,
+                              tooltip=columns[1],
+                              is_header=True)]
 
         rows = []
-        rows.append(header_row)
+        rows.extend(header_row)
 
         for key, subdf in groups.items():
 
@@ -194,11 +270,13 @@ def tabulate(dataframe, columns, callback, orientation='V'):
                          in get_groups(subdf, columns[1]).items()}
 
             rows.append(Row(depth + 1,
+                            is_singleton,
                             colnames=colnames,
                             cells=cells,
-                            label=key))
+                            label=key,
+                            tooltip=columns[0]))
 
-        rows.append(header_row)
+        rows.extend(header_row)
 
         return Table(depth, rows=rows)
 
@@ -225,7 +303,9 @@ def dump_style(font_family, max_depth, output):
     # font_size_increment = base_font_size - caption_size
 
     base_border_width = 1
-    base_padding = 15
+    # base_margin = 15
+    # base_padding = 15
+    base_spacing = 15
 
     print >> output, \
 '''
@@ -265,7 +345,10 @@ th, td:first-child, td:last-child {
     # font_size = base_font_size + font_size_increment
     font_size = base_font_size
     border_width = base_border_width
-    padding = base_padding
+
+    # padding = base_padding
+    # margin = base_margin
+    spacing = base_spacing
 
     styles = []
 
@@ -277,20 +360,20 @@ tr.depth-%(depth)d > td, tr.depth-%(depth)d > th {
   font-size: %(font_size)dpx;
 }
 
-tr.depth-%(depth)d > td:first-child {
-  padding-left: %(padding)dpx;
+tr.depth-%(depth)d > td:first-child > .header {
+  margin-left: %(spacing)dpx;
 }
 
-tr.depth-%(depth)d > td:last-child {
-  padding-right: %(padding)dpx;
+tr.depth-%(depth)d > td:last-child > .header {
+  margin-right: %(spacing)dpx;
 }
 
-tr.depth-%(depth)d:first-child > th {
-  padding-top: %(padding)dpx;
+tr.depth-%(depth)d:first-child > th > .header {
+  margin-top: %(spacing)dpx;
 }
 
-tr.depth-%(depth)d:last-child > th {
-  padding-bottom: %(padding)dpx;
+tr.depth-%(depth)d:last-child > th > .header {
+  margin-bottom: %(spacing)dpx;
 }
 
 tr.depth-%(depth)d > td:not(first-child):not(last-child) {
@@ -302,29 +385,11 @@ tr.depth-%(depth)d > td:not(first-child):not(last-child) {
         # font_size = font_size + font_size_increment
         font_size = 2 * font_size
         border_width = 2 * border_width
-        padding = 2 * padding
+        # padding = 2 * padding
+        # margin = 2 * margin
+        spacing = 2 * spacing
 
-
-def dump_epilogue(output):
-
-    print >> output, '''
-<div id="popup" class="overlay">
-  <div class="wrapper">
-    <div>
-      <div class="title-div"><span class="title"></span></div>
-      <img/>
-    </div>
-  </div>
-</div>
-
-<script src="//code.jquery.com/jquery-3.1.0.min.js"
-        integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
-        crossorigin="anonymous"></script>
-
-<script src="../../js/code.js"></script>
-'''
-
-def dump_table(dataframe, key_columns, callback, outputdir, **kwargs):
+def dump_table(dataframe, key_columns, callback, basedir, outputdir, **kwargs):
 
     misc.mkdirp(outputdir)
     output_filename = os.path.join(outputdir, 'index.html' )
@@ -340,7 +405,10 @@ def dump_table(dataframe, key_columns, callback, outputdir, **kwargs):
         # print >> output, ('''<link href="https://fonts.googleapis.com/css'''
         #                   '''?family=%s" rel="stylesheet">''' % font_family)
 
-        print >> output, '''<link rel="stylesheet" href="../../css/style.css"/>'''
+        css = os.path.relpath(os.path.join(basedir, 'css', 'style.css'),
+                              outputdir)
+
+        print >> output, '<link rel="stylesheet" href="%s"/>' % css
 
         print >> output, '<style>\n'
         dump_style(font_family, max_depth, output)
@@ -348,42 +416,103 @@ def dump_table(dataframe, key_columns, callback, outputdir, **kwargs):
 
         print >> output, table
 
-        dump_epilogue(output)
+        print >> output, '''
+<div id="popup">
+  <div class="title-div"><span class="title"></span></div><br/>
+  <img/>
+</div>
+
+<script src="//code.jquery.com/jquery-3.1.0.min.js"
+        integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+        crossorigin="anonymous"></script>
+'''
+        js = os.path.relpath(os.path.join(basedir, 'js', 'code.js'),
+                             outputdir)
+        print >> output, '<script src="%s"></script>' % js
 
 # -----------------------------------------------------------------------------
 
+def path_to_tuple(path):
+    return tuple(path.split(os.path.sep))
+
+def annotate_paths(paths):
+
+    def parse_metadata(path):
+
+        def _parse_metadata(path):
+
+            if path == '' or path == '.':
+                return []
+
+            if os.path.isdir(path):
+                factordir = os.path.join(path, '.factor')
+                if os.path.isdir(factordir):
+                    level = os.path.basename(path)
+                    factor = os.listdir(factordir)[0]
+                    pair = [(level, factor)]
+                else:
+                    pair = []
+            else:
+                pair = [(path, 'value')]
+
+            return _parse_metadata(os.path.dirname(path)) + pair
+
+        return zip(*_parse_metadata(path))
+
+    number_of_paths = len(paths)
+
+    rows, column_names = zip(*map(parse_metadata,
+                                  sorted(paths, key=path_to_tuple)))
+
+    keys = [tuple(row[:-1]) for row in rows]
+    number_of_keys = len(set(keys))
+
+    if ((number_of_paths != number_of_keys) or
+        len(set(column_names)) != 1):
+
+        raise ValueError('malformed input')
+
+    return make_dataframe(rows, column_names[0])
+
+
 def main():
-    inputbase = sys.argv[1]
-    outputdir = sys.argv[2]
+    import fileinput
 
-    exploded_paths = [path_.split('/')[:-1] +
-                      [os.path.join('..', '..', 'img', path_)]
-                      for path_ in get_files(inputbase, is_thumbnail)]
+    # if hasattr(sys, 'original_argv'):
+    #     sys.argv = sys.original_argv[:]
+    # else:
+    #     sys.original_argv = sys.argv[:]
 
-    column_names = 'number_of_genes expected_number_of_counts_per_gene method treatment'.split()
-    dataframe = make_dataframe(exploded_paths, column_names + ['value'])
+    basedir = sys.argv.pop(1)
+    outputdir = sys.argv.pop(1)
 
-    key_columns = 'number_of_genes expected_number_of_counts_per_gene treatment method'.split()
+    def convert(line):
+        path = line.rstrip('\r\n')
+        return path
+        # path = os.path.normpath(os.path.join(basedir, line.rstrip('\r\n')))
+        # return os.path.relpath(path, outputdir)
+
+    paths = map(convert, fileinput.input())
+    os.chdir(basedir)
+    annotated = annotate_paths(paths)
+
+    # annotated.to_csv(sys.stdout, sep='\t', index=False)
+    # return
+
+    key_columns = list(annotated.columns)[:-1]
+
     def callback(dataframe, depth):
         keys = dataframe.loc[:, key_columns].values[0]
-        value = dataframe.loc[:, 'value'].values[0]
-        return Img(depth, keys, value)
+        subpath = dataframe.loc[:, 'value'].values[0]
+        fullpath = os.path.join(basedir, subpath)
+        return Img(depth, keys, os.path.relpath(fullpath, outputdir))
 
-    dump_table(dataframe, key_columns, callback,
-               os.path.join(outputdir, 'plots', '__ALL__'),
-               orientation='H')
+    dump_table(annotated, key_columns, callback, basedir, outputdir, orientation='H')
 
-    # first_key_column = key_columns[0]
-    # trailing_key_columns = key_columns[1:]
 
-    # for channel, group in dataframe.groupby(first_key_column).groups.items():
-
-    #     subdataframe = dataframe.loc[group, :]
-    #     plotdir = os.path.join(outputdir, 'plots', channel)
-
-    #     dump_table(subdataframe,
-    #                ['tissue', 'status', 'day', 'replicate'],
-    #                callback,
-    #                plotdir)
-
-main()
+CURRENT_DIRECTORY = os.path.abspath(os.curdir)
+try:
+    main()
+except:
+    os.chdir(CURRENT_DIRECTORY)
+    raise
